1458379648ab967c6779215eed0cbb24b5902de5
Nikolay <mikolasan@users.noreply.github.com>
2015-02-19 11:44:07 +0500

    added encoding script

---
+++ pisem.net-encoding.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#
+
+FROM="utf-8"
+TO="win"
+
+if [ -d "$FROM" ]; then
+    # prepare hierarhy
+    if [ -d "$TO" ]; then
+        rm -r $TO
+    fi
+    mkdir $TO
+    cp -r $FROM/* $TO
+    find $TO -name "*.*" -type f -exec rm '{}' \;
+
+    # process
+    for ext in "*.html" "*.js" "*.css"; do
+        FILES=`find $FROM -type f -name "$ext" | xargs file | grep UTF-8 | cut -d ":" -f 1` # take html in utf 8 encoding
+        if [ -n "$FILES" ]; then
+            for file in $FILES; do
+                echo "change encoding $file > $TO/${file#*/}"
+                iconv -f utf-8 -t windows-1251 "$file" > "$TO/${file#*/}"
+            done
+        fi
+    done
+fi
+
+#
